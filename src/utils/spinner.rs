4f24d0841306fdaf4099670551d387f5f6910221
Michael Mandel <michael.mandel@awin.com>
2022-09-05 16:25:39 +0200

    chore(deps): upgrade dependencies (#42)
    * chore(deps): upgrade dependencies
    
    * chore(deps): adjust spinner builder to breaking changes from dependency upgrade
    
    * ci: upgrade codecov action version to v3
    
    * ci: upgrade checkout action version to v3

--- src/utils/spinner.rs
+++ src/utils/spinner.rs
@@ -1,12 +1,14 @@
+use std::time::Duration;
+
 use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn get_spinner() -> ProgressBar {
     let spinner = ProgressBar::new_spinner();
+    spinner.enable_steady_tick(Duration::from_millis(80));
     spinner.set_style(
-        ProgressStyle::default_spinner()
-            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"])
-            .template("{spinner:.blue} {msg}"),
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]),
     );
-    spinner.enable_steady_tick(80);
     spinner
 }
